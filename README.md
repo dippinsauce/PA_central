129631f2a41e8c63e756df776b7da0869a7355fa
dippinsauce <darrellwebb2@gmail.com>
2016-06-07 20:59:02 +0000

    second changes, AGAIN

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # PA_central
+
+
+
+
+
+Build a REST API on flask for data collection and controlling action.
+
+
+database: "lake"
+	table: "interior_temp"
+		column: "id" INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+		column: "timestamp" TIMESTAMP
+		column: "temp" FLOAT 
+		column: "humidity" FLOAT
+
+	table: "test"
+		column: "id" INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+		column: "timestamp" TIMESTAMP
+		column: "test_float" FLOAT
+		column: "test_int" INT
+
+		
--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="dippinsauce",
+    password="jyrqskrM1D9M2NAu0atF",
+    hostname="dippinsauce.mysql.pythonanywhere-services.com",
+    databasename="dippinsauce$lake",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
 
+#Start the restful API routes
+@app.route('/dbase/')
+def put_database():
+    return 'getting started with dbase rest api'
+
+@app.route('/wibble/')
+def wibble():
+    return 'nothing interesting here'
